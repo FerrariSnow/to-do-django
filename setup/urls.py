16e5744805bd25c55ded241aec0a162f2f0ccccf
Ferrari <ferraridev18@gmail.com>
2023-10-23 15:57:31 -0300

    alterado a listagem de tarefas de FBV para CBV

--- setup/urls.py
+++ setup/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
 
-from todos.views import todo_list
+from todos.views import TodoListView
 
-urlpatterns = [path("admin/", admin.site.urls), path("", todo_list)]
+urlpatterns = [path("admin/", admin.site.urls), path("", TodoListView.as_view())]
